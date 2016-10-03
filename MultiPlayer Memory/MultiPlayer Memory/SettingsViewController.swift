4e83cbb0390858e7fddb02a2b7b6bdebf010d3ce
Fredrik Holmdahl <freholmd@student.chalmers.se>
2016-10-03 19:55:04 +0200

    Fixed navigation and started with other views. Will create new branch for those next

---
+++ MultiPlayer Memory/MultiPlayer Memory/SettingsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MultiPlayer Memory
+//
+//  Created by Magnus Huttu on 03/10/16.
+//  Copyright © 2016 Magnus Huttu. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
